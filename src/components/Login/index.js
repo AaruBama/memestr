fa3a4a1d7578629c6c841fb30f0c1242e0c42d98
aarubama <aarubuibaama@gmail.com>
2023-08-12 17:46:53 +0530

    Add logout functionality

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -23,10 +23,20 @@ function Login(props) {
     const handleLoginSubmit = (event) => {
         event.preventDefault();
         let status = false
-        let userDetails = getUserDetailsFromPrivateKey(privateKey)
+        let userDetails = null;
+        const storedData = localStorage.getItem('memestr')
+        if (storedData) {
+            userDetails = JSON.parse(storedData);
+            setShowLogin(false);
+            setisLoggedIn(true);
+            const display_name = value.display_name
+            const profile_picture = value.picture
+            setLoggedInUser({ display_name, profile_picture });
+        } else {
+            userDetails = getUserDetailsFromPrivateKey(privateKey)
+        }
         console.log("user details are", userDetails)
         userDetails.then((value) => {
-
             status = true
             const display_name = value.display_name
             const profile_picture = value.picture
@@ -34,9 +44,17 @@ function Login(props) {
             setShowLogin(false);
             setisLoggedIn(true);
             setLoggedInUser({ display_name, profile_picture });
+            localStorage.setItem('memestr', JSON.stringify(value));
         });
 
     };
+
+    function logoutUser() {
+        localStorage.removeItem('memestr');
+        setShowLogin(true);
+        setisLoggedIn(false);
+        alert("Logged out successfully!")
+    }
 
     return (
         <div className="Login">
@@ -45,6 +63,7 @@ function Login(props) {
                     <div>
                         <img className='profile1' src={loggedInUser.profile_picture} alt="Profile" />
                         <div className='username'>Welcome, {loggedInUser.display_name}</div>
+                        <button className='logout' onClick={logoutUser}>Logout</button>
                     </div>
                     :
                     <button onClick={handleLoginClick}>Login</button>
