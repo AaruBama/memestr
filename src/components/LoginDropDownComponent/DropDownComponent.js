693bd1ee36d0084ad460112c6895a026f17fc1d4
RV <aarubuibaama@gmail.com>
2023-09-09 08:19:48 +0530

    fix the login nav bar

--- src/components/LoginDropDownComponent/DropDownComponent.js
+++ src/components/LoginDropDownComponent/DropDownComponent.js
@@ -78,7 +78,12 @@ function DropdownComponent() {
                             (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>) :
-                            (<img src={userDetails.picture} alt={""} class={"rounded"} />)
+                            <div className="flex flex-wrap justify-center w-12 h-12">
+                                <div className="px-4">
+                                    {console.log(userDetails.picture)}
+                                    <img src={userDetails} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                                </div>
+                            </div>
                         }
                     </Menu.Button>
                 </div>
--- src/components/LoginDropDownComponent/LoginModal.js
+++ src/components/LoginDropDownComponent/LoginModal.js
@@ -20,8 +20,11 @@ function LoginModal({ isOpen, onClose}) {
             const profile_picture = userDetails.picture
             setLoggedInUserDetails({"display_name": display_name, "picture": profile_picture,"name": name});
         } else {
+            console.log('Fetching login Details')
             userDetails = getUserDetailsFromPrivateKey(privateKey)
+            console.log('Fetched user Details', userDetails)
             userDetails.then((value) => {
+                console.log("value is ", value)
                 const display_name = value.display_name
                 const profile_picture = value.picture
                 const name = value.name
@@ -31,6 +34,7 @@ function LoginModal({ isOpen, onClose}) {
                 value["pubKey"] = publicKey
                 value["privateKey"] = privateKey //Encrypt it.
                 localStorage.setItem('memestr', JSON.stringify(value));
+                onClose(loggedInUserDetails)
             })
         }
 
@@ -91,7 +95,7 @@ function LoginModal({ isOpen, onClose}) {
                                         className="mt-2 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                                         onClick={() => {
                                             logUserIn();
-                                            onClose(loggedInUserDetails);}
+                                            }
                                     }
                                     >
                                         Login
