32f33030637bd578e266a170ddfb0559749649e6
RV <aarubuibaama@gmail.com>
2023-10-02 20:29:33 +0530

    Change loader

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -11,19 +11,12 @@ function Spinner() {
                 justifyContent: "center",
                 height: "100vh",
             }}>
-            <div className="lds-ring">
+            <div class="lds-ellipsis">
                 <div></div>
                 <div></div>
                 <div></div>
                 <div></div>
             </div>
-            {/*<div*/}
-            {/*    className="m-12 inline-block h-8 w-8 text-white animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"*/}
-            {/*    role="status">*/}
-            {/*    <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">*/}
-            {/*        .*/}
-            {/*    </span>*/}
-            {/*</div>*/}
         </div>
     );
 }
