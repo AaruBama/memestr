bb364038c7100787bedafe0352664bc5b8364625
appala venkata avinash <aa760@snu.edu.in>
2024-01-10 16:02:09 +0530

    Fixed app.js and providers issue

--- src/App.js
+++ src/App.js
@@ -1,68 +1,42 @@
 import './App.css';
-import { HashTagToolProvider } from './components/HashtagTool';
-import PostViewTool from './components/Post/post.js';
-import HeaderBar from './components/Login';
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-
 import HashtagTool from './components/HashtagTool';
-import PetFeed, { PetsFeedProvider } from './components/CategoryView/PetFeed';
-import NatureFeed, {
-    NatureProvider,
-} from './components/CategoryView/NatureProvider';
-// import BestForMobile from './components/Menu/BestForMobile';
+import PostViewTool from './components/Post/post.js';
+import HeaderBar from './components/Login';
+import { HashTagToolProvider } from './components/HashtagTool';
 
 function App() {
     const pageFilters = {
-        '/': null, // Default filters (null or any other default filters you want to use)
+        '/': null,
         '/pets': ['pets', 'dogs', 'cats'],
         '/nature': ['nature', 'flowers', 'sky', 'sea'],
         '/food': ['food'],
         '/photography': ['photography'],
     };
 
-    // const location = useLocation();
+    const renderHashtagToolRoutes = () => {
+        return Object.entries(pageFilters).map(([path, filterTags]) => (
+            <Route
+                key={path}
+                path={path}
+                element={
+                    <HashTagToolProvider filterTags={filterTags}>
+                        <HashtagTool />
+                    </HashTagToolProvider>
+                }
+            />
+        ));
+    };
 
     return (
-        <div>
-            {/*<BestForMobile />*/}
-            <Router>
-                <HeaderBar />
-                <HashTagToolProvider>
-                    <Routes>
-                        <Route exact path="/" element={<HashtagTool />} />
-                        <Route
-                            path="/post/:postId"
-                            element={<PostViewTool />}
-                        />
-                    </Routes>
-                </HashTagToolProvider>
-                <PetsFeedProvider filterTags={pageFilters['/pets']}>
-                    <Routes>
-                        <Route exact path="/pets" element={<PetFeed />} />
-                    </Routes>
-                </PetsFeedProvider>
-                <NatureProvider filterTags={pageFilters['/nature']}>
-                    <Routes>
-                        <Route exact path="/nature" element={<NatureFeed />} />
-                    </Routes>
-                </NatureProvider>
-                <NatureProvider filterTags={pageFilters['/food']}>
-                    <Routes>
-                        <Route exact path="/food" element={<NatureFeed />} />
-                    </Routes>
-                </NatureProvider>
-                <NatureProvider filterTags={pageFilters['/photography']}>
-                    <Routes>
-                        <Route
-                            exact
-                            path="/photography"
-                            element={<NatureFeed />}
-                        />
-                    </Routes>
-                </NatureProvider>
-            </Router>
-        </div>
+        <Router>
+            <HeaderBar />
+            <Routes>
+                {renderHashtagToolRoutes()}
+                <Route path="/post/:postId" element={<PostViewTool />} />
+            </Routes>
+        </Router>
     );
 }
 
--- src/components/CategoryView/NatureProvider.js
+++ src/components/CategoryView/NatureProvider.js
@@ -1,218 +1,218 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { SimplePool } from 'nostr-tools';
-import Feed from '../Feed';
-import PostUpload from '../Post/newPost';
-import Sidebar from '../HashtagTool/SideBar';
-import { ReactComponent as UploadSvg } from '../../Icons/UploadSvg.svg';
+// import React, { useContext, useEffect, useState } from 'react';
+// import { SimplePool } from 'nostr-tools';
+// import Feed from '../Feed';
+// import PostUpload from '../Post/newPost';
+// import Sidebar from '../HashtagTool/SideBar';
+// import { ReactComponent as UploadSvg } from '../../Icons/UploadSvg.svg';
 
-const relays = [
-    'wss://relay.damus.io/',
-    // 'wss://offchain.pub/',
-    // 'wss://nos.lol/',
-    // 'wss://relay.nostr.wirednet.jp/',
-    // 'wss://nostr.wine/',
-];
+// const relays = [
+//     'wss://relay.damus.io/',
+//     // 'wss://offchain.pub/',
+//     // 'wss://nos.lol/',
+//     // 'wss://relay.nostr.wirednet.jp/',
+//     // 'wss://nostr.wine/',
+// ];
 
-const CategorizedContext = React.createContext();
+// const CategorizedContext = React.createContext();
 
-export function NatureProvider({ children, filterTags }) {
-    const [notes, setNotes] = useState([]);
-    const [lastCreatedAt, setLastCreatedAt] = useState();
-    const [scrollPosition, setScrollPosition] = useState(0);
-    const [isLoading, setIsLoading] = useState(false);
+// export function NatureProvider({ children, filterTags }) {
+//     const [notes, setNotes] = useState([]);
+//     const [lastCreatedAt, setLastCreatedAt] = useState();
+//     const [scrollPosition, setScrollPosition] = useState(0);
+//     const [isLoading, setIsLoading] = useState(false);
 
-    const containsJpgOrMp4Link = text => {
-        const linkRegex = /(https?:\/\/[^\s]+(\.jpg|\.mp4|\.gif))/gi;
-        return linkRegex.test(text);
-    };
+//     const containsJpgOrMp4Link = text => {
+//         const linkRegex = /(https?:\/\/[^\s]+(\.jpg|\.mp4|\.gif))/gi;
+//         return linkRegex.test(text);
+//     };
 
-    async function getVotes(postIds) {
-        const voteFilters = {
-            kinds: [7],
-            '#e': postIds,
-        };
-        const relayPool = new SimplePool();
-        let votes = await relayPool.list(relays, [voteFilters]);
-        const groupedByPostId = {};
+//     async function getVotes(postIds) {
+//         const voteFilters = {
+//             kinds: [7],
+//             '#e': postIds,
+//         };
+//         const relayPool = new SimplePool();
+//         let votes = await relayPool.list(relays, [voteFilters]);
+//         const groupedByPostId = {};
 
-        for (const vote of votes) {
-            const voteTags = vote.tags;
-            for (const tag of voteTags) {
-                if (tag[0] === 'e') {
-                    if (groupedByPostId[tag[1]]) {
-                        groupedByPostId[tag[1]] += 1;
-                    } else {
-                        groupedByPostId[tag[1]] = 1;
-                    }
-                }
-            }
-        }
-        return groupedByPostId;
-    }
+//         for (const vote of votes) {
+//             const voteTags = vote.tags;
+//             for (const tag of voteTags) {
+//                 if (tag[0] === 'e') {
+//                     if (groupedByPostId[tag[1]]) {
+//                         groupedByPostId[tag[1]] += 1;
+//                     } else {
+//                         groupedByPostId[tag[1]] = 1;
+//                     }
+//                 }
+//             }
+//         }
+//         return groupedByPostId;
+//     }
 
-    useEffect(() => {
-        const LoadMedia = async () => {
-            // Fetch notes and update the context state
-            // ...
-            setIsLoading(true);
-            const relayPool = new SimplePool();
-            const filters = {
-                limit: 20,
-            };
+//     useEffect(() => {
+//         const LoadMedia = async () => {
+//             // Fetch notes and update the context state
+//             // ...
+//             setIsLoading(true);
+//             const relayPool = new SimplePool();
+//             const filters = {
+//                 limit: 20,
+//             };
 
-            // For Memes
-            if (filterTags) {
-                filters['#t'] = filterTags;
-            } else {
-                filters['#t'] = ['memes', 'meme', 'funny', 'memestr'];
-            }
-            let notes = await relayPool.list(relays, [filters]);
-            notes = notes.filter(note => {
-                return containsJpgOrMp4Link(note.content);
-            });
-            // console.log("notes are ", notes)
-            // GET VOTES COMBINED
-            let createdAt = [];
-            let postIds = [];
-            notes.forEach(function (note) {
-                var id = note.id;
-                createdAt.push(note.created_at);
-                postIds.push(id);
-            });
-            createdAt.sort(function (a, b) {
-                return a - b;
-            });
+//             // For Memes
+//             if (filterTags) {
+//                 filters['#t'] = filterTags;
+//             } else {
+//                 filters['#t'] = ['memes', 'meme', 'funny', 'memestr'];
+//             }
+//             let notes = await relayPool.list(relays, [filters]);
+//             notes = notes.filter(note => {
+//                 return containsJpgOrMp4Link(note.content);
+//             });
+//             // console.log("notes are ", notes)
+//             // GET VOTES COMBINED
+//             let createdAt = [];
+//             let postIds = [];
+//             notes.forEach(function (note) {
+//                 var id = note.id;
+//                 createdAt.push(note.created_at);
+//                 postIds.push(id);
+//             });
+//             createdAt.sort(function (a, b) {
+//                 return a - b;
+//             });
 
-            let groupedByPostId = await getVotes(postIds);
-            for (const note of notes) {
-                note['voteCount'] = groupedByPostId[note.id] || 0;
-            }
-            setNotes(notes);
-            setLastCreatedAt(createdAt[0]);
-            setIsLoading(false);
-            relayPool.close(relays);
-        };
-        LoadMedia();
-    }, [filterTags]);
+//             let groupedByPostId = await getVotes(postIds);
+//             for (const note of notes) {
+//                 note['voteCount'] = groupedByPostId[note.id] || 0;
+//             }
+//             setNotes(notes);
+//             setLastCreatedAt(createdAt[0]);
+//             setIsLoading(false);
+//             relayPool.close(relays);
+//         };
+//         LoadMedia();
+//     }, [filterTags]);
 
-    const LoadMoreMedia = async () => {
-        // Fetch more notes with offset and update the context state
-        // ...
+//     const LoadMoreMedia = async () => {
+//         // Fetch more notes with offset and update the context state
+//         // ...
 
-        const relayPool = new SimplePool();
-        const filters = {
-            limit: 20,
-        };
+//         const relayPool = new SimplePool();
+//         const filters = {
+//             limit: 20,
+//         };
 
-        const relays = [
-            'wss://relay.damus.io/',
-            'wss://offchain.pub/',
-            'wss://nos.lol/',
-            'wss://relay.nostr.wirednet.jp/',
-            'wss://nostr.wine/',
-        ];
+//         const relays = [
+//             'wss://relay.damus.io/',
+//             'wss://offchain.pub/',
+//             'wss://nos.lol/',
+//             'wss://relay.nostr.wirednet.jp/',
+//             'wss://nostr.wine/',
+//         ];
 
-        // For Memes
-        if (filterTags) {
-            filters['#t'] = filterTags;
-        } else {
-            filters['#t'] = ['memes', 'meme', 'funny', 'memestr'];
-        }
+//         // For Memes
+//         if (filterTags) {
+//             filters['#t'] = filterTags;
+//         } else {
+//             filters['#t'] = ['memes', 'meme', 'funny', 'memestr'];
+//         }
 
-        filters['until'] = lastCreatedAt - 5 * 60;
+//         filters['until'] = lastCreatedAt - 5 * 60;
 
-        let newNotes = await relayPool.list(relays, [filters]);
-        newNotes = newNotes.filter(note => {
-            return containsJpgOrMp4Link(note.content);
-        });
+//         let newNotes = await relayPool.list(relays, [filters]);
+//         newNotes = newNotes.filter(note => {
+//             return containsJpgOrMp4Link(note.content);
+//         });
 
-        let createdAt = [];
-        let postIds = [];
-        newNotes.forEach(function (note) {
-            var id = note.id;
-            createdAt.push(note.created_at);
-            postIds.push(id);
-        });
-        createdAt.sort(function (a, b) {
-            return a - b;
-        });
-        let groupedByPostId = await getVotes(postIds);
-        for (const note of newNotes) {
-            note['voteCount'] = groupedByPostId[note.id] || 0;
-        }
-        setNotes(notes => [...notes, ...newNotes]);
-        setLastCreatedAt(createdAt[0]);
-        relayPool.close(relays);
-    };
+//         let createdAt = [];
+//         let postIds = [];
+//         newNotes.forEach(function (note) {
+//             var id = note.id;
+//             createdAt.push(note.created_at);
+//             postIds.push(id);
+//         });
+//         createdAt.sort(function (a, b) {
+//             return a - b;
+//         });
+//         let groupedByPostId = await getVotes(postIds);
+//         for (const note of newNotes) {
+//             note['voteCount'] = groupedByPostId[note.id] || 0;
+//         }
+//         setNotes(notes => [...notes, ...newNotes]);
+//         setLastCreatedAt(createdAt[0]);
+//         relayPool.close(relays);
+//     };
 
-    // Store the context value
-    const contextValue = {
-        notes,
-        scrollPosition,
-        setScrollPosition,
-        LoadMoreMedia,
-        isLoading,
-    };
+//     // Store the context value
+//     const contextValue = {
+//         notes,
+//         scrollPosition,
+//         setScrollPosition,
+//         LoadMoreMedia,
+//         isLoading,
+//     };
 
-    return (
-        <CategorizedContext.Provider value={contextValue}>
-            {children}
-        </CategorizedContext.Provider>
-    );
-}
+//     return (
+//         <CategorizedContext.Provider value={contextValue}>
+//             {children}
+//         </CategorizedContext.Provider>
+//     );
+// }
 
-export function useHashTagContext() {
-    const context = useContext(CategorizedContext);
-    if (!context) {
-        throw new Error(
-            'useHashTagContext must be used within a HashTagToolProvider',
-        );
-    }
-    return context;
-}
+// export function useHashTagContext() {
+//     const context = useContext(CategorizedContext);
+//     if (!context) {
+//         throw new Error(
+//             'useHashTagContext must be used within a HashTagToolProvider',
+//         );
+//     }
+//     return context;
+// }
 
-function NatureFeed() {
-    const { notes, LoadMoreMedia, isLoading } = useHashTagContext();
-    const [newPostModal, setNewPostModal] = useState(false);
+// function NatureFeed() {
+//     const { notes, LoadMoreMedia, isLoading } = useHashTagContext();
+//     const [newPostModal, setNewPostModal] = useState(false);
 
-    function showNewPostModal() {
-        setNewPostModal(true);
-    }
+//     function showNewPostModal() {
+//         setNewPostModal(true);
+//     }
 
-    function closePostModal() {
-        setNewPostModal(false);
-    }
+//     function closePostModal() {
+//         setNewPostModal(false);
+//     }
 
-    return (
-        <>
-            <div className="flex flex-col md:flex-row min-h-screen">
-                <Sidebar />
-                <main className="flex-1 overflow-y-auto">
-                    <Feed
-                        notes={notes}
-                        onLoadMore={LoadMoreMedia}
-                        isLoading={isLoading}
-                    />
+//     return (
+//         <>
+//             <div className="flex flex-col md:flex-row min-h-screen">
+//                 <Sidebar />
+//                 <main className="flex-1 overflow-y-auto">
+//                     <Feed
+//                         notes={notes}
+//                         onLoadMore={LoadMoreMedia}
+//                         isLoading={isLoading}
+//                     />
 
-                    <button
-                        onClick={() => {
-                            showNewPostModal();
-                        }}
-                        title="Upload"
-                        className="fixed z-10 bottom-4 right-3 md:right-8 bg-gradient-to-r from-blue-500 to-teal-500 hover:from-pink-500 hover:to-yellow-500 w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-white hover:drop-shadow-2xl hover:animate-bounce duration-300">
-                        <UploadSvg />
-                    </button>
+//                     <button
+//                         onClick={() => {
+//                             showNewPostModal();
+//                         }}
+//                         title="Upload"
+//                         className="fixed z-10 bottom-4 right-3 md:right-8 bg-gradient-to-r from-blue-500 to-teal-500 hover:from-pink-500 hover:to-yellow-500 w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-white hover:drop-shadow-2xl hover:animate-bounce duration-300">
+//                         <UploadSvg />
+//                     </button>
 
-                    {newPostModal && (
-                        <PostUpload
-                            isOpen={newPostModal}
-                            onClose={closePostModal}
-                        />
-                    )}
-                </main>
-            </div>
-        </>
-    );
-}
+//                     {newPostModal && (
+//                         <PostUpload
+//                             isOpen={newPostModal}
+//                             onClose={closePostModal}
+//                         />
+//                     )}
+//                 </main>
+//             </div>
+//         </>
+//     );
+// }
 
-export default NatureFeed;
+// export default NatureFeed;
