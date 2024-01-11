9deb9048f0b2648383e5d02bd841d8587438863c
appala venkata avinash <aa760@snu.edu.in>
2024-01-11 21:15:24 +0530

    added pause button to video and adjusted z-index

--- src/helpers/videoPlayer.js
+++ src/helpers/videoPlayer.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { ReactComponent as VolumeUp } from '../Icons/volumeup.svg';
 import { ReactComponent as VolumeDown } from '../Icons/VolumeDown.svg';
+import { ReactComponent as Play } from '../Icons/playButton.svg';
 
 export function VideoPlayer({ imageLink }) {
     const [muted, setMuted] = useState(true);
+    const [isPlaying, setIsPlaying] = useState(false);
     const videoRef = useRef(null);
 
     const handleMuteToggle = () => {
@@ -13,8 +15,10 @@ export function VideoPlayer({ imageLink }) {
     const togglePlayPause = () => {
         if (videoRef.current.paused || videoRef.current.ended) {
             videoRef.current.play();
+            setIsPlaying(true);
         } else {
             videoRef.current.pause();
+            setIsPlaying(false);
         }
     };
 
@@ -24,12 +28,14 @@ export function VideoPlayer({ imageLink }) {
                 const entry = entries[0];
                 if (entry.isIntersecting) {
                     videoRef.current.play();
+                    setIsPlaying(true);
                 } else {
                     videoRef.current.pause();
+                    setIsPlaying(false);
                 }
             },
             {
-                threshold: 0.5, // 50% of the video must be in the viewport to play
+                threshold: 0.5,
             },
         );
 
@@ -41,7 +47,7 @@ export function VideoPlayer({ imageLink }) {
     }, []);
 
     return (
-        <div className="relative w-full max-w-screen-md mx-auto">
+        <div className="relative w-full max-w-screen-md mx-auto z-30">
             <video
                 ref={videoRef}
                 className="w-full h-auto rounded-lg overflow-hidden shadow-lg bg-black"
@@ -49,10 +55,16 @@ export function VideoPlayer({ imageLink }) {
                 muted={muted}
                 playsInline
                 loop
-                onClick={togglePlayPause} // Added onClick event handler
-            >
+                onClick={togglePlayPause}>
                 <source src={imageLink} type="video/mp4" />
             </video>
+            {!isPlaying && (
+                <div
+                    className="absolute top-0 left-0 w-full h-full flex items-center justify-center"
+                    onClick={togglePlayPause}>
+                    <Play className="h-12 w-12" />
+                </div>
+            )}
             <button
                 onClick={handleMuteToggle}
                 className="absolute bottom-4 right-4 bg-white p-2 rounded-full shadow-lg focus:outline-none">
